1d1389c0d0e9ad788ca40ca2b3c678e0caad77c3
Sono <sono.gomorrha@gmail.com>
2022-12-11 20:10:22 +0100

    switched around some tings and added ED layer

--- keyboards/handwired/12plus1/config.h
+++ keyboards/handwired/12plus1/config.h
@@ -24,8 +24,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Handwired
-#define PRODUCT         12plus1 Macropad
+#define MANUFACTURER    "Handwired"
+#define PRODUCT         "12plus1 Macropad"
 
 /* key matrix size */
 #define MATRIX_ROWS 3
